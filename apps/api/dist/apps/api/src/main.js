7acc6546be92071a08addfd9a0e178306421f49c
Smart Pickup <deploy@smartpickup.sa>
2026-05-31 02:28:14 +0300

    fix: add module-alias to resolve @smart-pickup/shared at runtime

--- apps/api/dist/apps/api/src/main.js
+++ apps/api/dist/apps/api/src/main.js
@@ -1,4 +1,5 @@
 "use strict";
+require("module-alias/register");
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
